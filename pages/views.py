803202f5569ab61a2634866ec6fe157fa3590291
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-03-05 05:51:01 +0100

    returned a string of html page in homePageView

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,26 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homePageView(request):
-    return HttpResponse('Hello, World!')
+    answer = '''
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Django Hello,World Website</title>
+        </head>
+        <style>
+            body {
+                font-family: Arial;
+            }
+            h1 {
+                color: red;
+                font-weight: lighter;
+            }
+
+        </style>
+        <body>
+            <h1> Hello, World </h1>
+        </body>
+    </html>
+    
+    '''
+    return HttpResponse(answer)
